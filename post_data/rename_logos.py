327c662bb8ccb48aeb2dbfb2ca8b3d8fb033b8b9
Andreas Calleja <andreascalleja@gmail.com>
2023-01-23 22:11:51 +0100

    Uploaded France logos

--- post_data/rename_logos.py
+++ post_data/rename_logos.py
@@ -1,25 +1,24 @@
 import os
 
 club_list = [
-    "Almeria",
-    "Bilbao",
-    "Atletico",
-    "Barcelona",
-    "Cadiz",
-    "Celta",
-    "Elche",
-    "Espanyol",
-    "Getafe",
-    "Girona",
-    "Mallorca",
-    "Osasuna",
-    "Rayo",
-    "Real Madrid" "Betis",
-    "Sociedad",
-    "Seville",
-    "Valencia",
-    "Valladolid",
-    "Villarreal",
+    "ajaccio",
+    "angers",
+    "auxerre",
+    "brest",
+    "clermont",
+    "lens",
+    "lille",
+    "lorient",
+    "lyon",
+    "marseille",
+    "nantes",
+    "nice",
+    "PSG",
+    "reims",
+    "rennes",
+    "strasbourg",
+    "toulouse",
+    "troyes",
 ]
 
 logos_dir = (
@@ -38,7 +37,7 @@ for index, club in enumerate(club_list):
     if partial(list_logos, club):
         logo = partial(list_logos, club)[0]
         logo_new_name = logo.split(".")
-        logo_new_name[0] = str(index + 41)
+        logo_new_name[0] = str(index + 61)
         logo_new_name = ".".join(logo_new_name)
         print(logo)
         print(logo_new_name)
